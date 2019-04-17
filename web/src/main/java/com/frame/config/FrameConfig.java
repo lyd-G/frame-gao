5f44ff039384d2fceb15589a55dbfe3b3d7ae75f
yu <laiyu.gao@qq.com>
2019-04-17 09:31:16 +0800

    init01

---
+++ web/src/main/java/com/frame/config/FrameConfig.java
@@ -0,0 +1,21 @@
+package com.frame.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "frame")
+public class FrameConfig {
+    private String zipPassword;
+    private String redisPassword;
+
+    @Override
+    public String toString() {
+        return "FrameConfig{" +
+                "zipPassword='" + zipPassword + '\'' +
+                ", redisPassword='" + redisPassword + '\'' +
+                '}';
+    }
+}
